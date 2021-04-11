ce8768cd87d05d283c62f825ee0e4c9bbc00156c
Victor A <viktoray007@gmail.com>
2021-04-11 04:33:50 +0300

    show realtime balance

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase'
+
+import 'firebase/auth'
+import { FIREBASE_APP } from './constants/Firebase'
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyBl10QY9YiNIjbOvghjZJfNDZof_fB3Nrg',
+  authDomain: 'block-tracker-9c0b2.firebaseapp.com',
+  projectId: 'block-tracker-9c0b2',
+  storageBucket: 'block-tracker-9c0b2.appspot.com',
+  messagingSenderId: '545142564282',
+  appId: '1:545142564282:web:b991cbddd99f645c9f6983',
+  measurementId: 'G-2B914Y556J'
+}
+
+if (!firebase) {
+  firebase.initializeApp(firebaseConfig, FIREBASE_APP)
+} else {
+  firebase.app(FIREBASE_APP)
+}
